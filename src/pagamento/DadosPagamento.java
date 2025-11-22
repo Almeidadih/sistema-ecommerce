10aff0df25e118b740f43b933f88235f7e99ba5f
diego <almeidadieo488@gmail.com>
2025-11-22 18:45:30 -0300

    Finalizando a tarefa de inserção da classe Pedidos

---
+++ src/pagamento/DadosPagamento.java
@@ -0,0 +1,50 @@
+package pagamento;
+
+import dominio.DadosCartao;
+
+import java.util.Optional;
+
+public class DadosPagamento {
+
+    private final TipoPagamento tipoPagamento;
+    private final DadosCartao dadosCartao;
+    private final Integer quantidadeParcelas;
+    private final String chavePix;
+
+
+    public DadosPagamento(TipoPagamento tipoPagamento, DadosCartao dadosCartao, Integer quantidadeParcelas) {
+        this.tipoPagamento = tipoPagamento;
+        this.dadosCartao = dadosCartao;
+        this.quantidadeParcelas = quantidadeParcelas;
+        this.chavePix = null;
+    }
+
+    public DadosPagamento(TipoPagamento tipoPagamento,String chavePix) {
+        this.tipoPagamento = tipoPagamento;
+        this.dadosCartao = null;
+        this.quantidadeParcelas = 1;
+        this.chavePix = chavePix;
+    }
+
+    public DadosPagamento(TipoPagamento tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
+        this.dadosCartao = null;
+        this.quantidadeParcelas = 1;
+        this.chavePix = null;
+    }
+
+    public TipoPagamento getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public Optional<DadosCartao> getDadosCartao(){
+        return Optional.ofNullable(dadosCartao);
+    }
+
+    public Integer getQuantidadeParcelas(){
+        return quantidadeParcelas;
+    }
+    public Optional<String> getChavePix(){
+        return Optional.ofNullable(chavePix);
+    }
+}
--- src/pagamento/PedidoService.java
+++ src/pagamento/PedidoService.java
@@ -1,4 +1,36 @@
 package pagamento;
 
+import dominio.Pedido;
+import frete.FreteService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.sql.DriverManager.println;
+
 public class PedidoService {
+
+    private static final List<Pedido> PEDIDOS = new ArrayList<>();
+
+    public static void efetuarPedido(Pedido pedido) {
+
+        {
+            println("\n📦 Iniciando criação do pedido: " + pedido);
+            println(" Total: R$ " + pedido.calcularSubtotal());
+        }
+
+        DescontoService.aplicarDesconto(pedido);
+        FreteService.aplicarFrete(pedido);
+        PEDIDOS.add(pedido);
+        {
+            println("\n📦 Pedido criado: " + pedido);
+            println(" Subtotal: R$ " + pedido.calcularSubtotal());
+        }
+        PagamentoService.processarPagamento(pedido);
+        NotificacaoService.enviarNotificacoes(pedido);
+    }
+
+    public static List<Pedido> findQPedidos() {
+        return PEDIDOS;
+    }
 }
